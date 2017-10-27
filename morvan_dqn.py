6214bdaba95119bc98db842fb09a7d1ac3612b63
JIElite <ita3051@gmail.com>
2017-10-27 22:33:45 +0800

    add morvan version

---
+++ morvan_dqn.py
@@ -0,0 +1,133 @@
+"""
+View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
+My Youtube Channel: https://www.youtube.com/user/MorvanZhou
+More about Reinforcement learning: https://morvanzhou.github.io/tutorials/machine-learning/reinforcement-learning/
+
+Dependencies:
+torch: 0.2
+gym: 0.8.1
+numpy
+"""
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+import gym
+
+# Hyper Parameters
+BATCH_SIZE = 64
+LR = 0.005                   # learning rate
+LR_DECAY = 0.01             
+EPSILON = 1.0               # greedy policy
+EPSILON_DECAY = 0.995
+MIN_EPSILON = 0.05
+GAMMA = 0.9                 # reward discount
+TARGET_REPLACE_ITER = 300   # target update frequency
+MEMORY_CAPACITY = 100000
+env = gym.make('CartPole-v0')
+env = env.unwrapped
+N_ACTIONS = env.action_space.n
+N_STATES = env.observation_space.shape[0]
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(N_STATES, 24)
+        self.fc2 = nn.Linear(24, 48)
+        self.out = nn.Linear(48, N_ACTIONS)
+
+    def forward(self, x):
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        actions_value = self.out(x)
+        return actions_value
+
+
+class DQN(object):
+    def __init__(self):
+        self.eval_net, self.target_net = Net(), Net()
+        self.epsilon = EPSILON
+        self.epsilon_decay = EPSILON_DECAY
+        self.min_epsilon = MIN_EPSILON
+        self.learn_step_counter = 0                                     # for target updating
+        self.memory_counter = 0                                         # for storing memory
+        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 2))     # initialize memory
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR, weight_decay=LR_DECAY)
+
+        self.loss_func = nn.MSELoss()
+
+    def decay_epsilon(self):
+        new_epsilon = self.epsilon * self.epsilon_decay
+        if new_epsilon < self.min_epsilon:
+            self.epsilon = self.min_epsilon
+        else:
+            self.epsilon = new_epsilon
+
+    def choose_action(self, x):
+        x = Variable(torch.unsqueeze(torch.FloatTensor(x), 0))
+        # input only one sample
+        if np.random.uniform() > EPSILON:   # greedy
+            actions_value = self.eval_net.forward(x)
+            action = torch.max(actions_value, 1)[1].data.numpy()[0]     # return the argmax
+        else:   # random
+            action = np.random.randint(0, N_ACTIONS)
+        return action
+
+    def store_transition(self, s, a, r, s_):
+        transition = np.hstack((s, [a, r], s_))
+        # replace the old memory with new memory
+        index = self.memory_counter % MEMORY_CAPACITY
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+
+    def learn(self):
+        # target parameter update
+        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
+            self.target_net.load_state_dict(self.eval_net.state_dict())
+        self.learn_step_counter += 1
+
+        # sample batch transitions
+        sampled_length = self.learn_step_counter if self.learn_step_counter < MEMORY_CAPACITY else MEMORY_CAPACITY
+        sample_index = np.random.choice(sampled_length, BATCH_SIZE)
+        b_memory = self.memory[sample_index, :]
+        b_s = Variable(torch.FloatTensor(b_memory[:, :N_STATES]))
+        b_a = Variable(torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int)))
+        b_r = Variable(torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2]))
+        b_s_ = Variable(torch.FloatTensor(b_memory[:, -N_STATES:]))
+
+        # q_eval w.r.t the action in experience
+        q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)
+        q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
+        q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
+        loss = self.loss_func(q_eval, q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+dqn = DQN()
+
+print('\nCollecting experience...')
+for i_episode in range(3000):
+    s = env.reset()
+    ep_r = 0
+    while True:
+        # env.render()
+        a = dqn.choose_action(s)
+        # take action
+        s_, r, done, info = env.step(a)
+
+        # modify the reward
+        dqn.store_transition(s, a, r, s_)
+
+        ep_r += r
+        dqn.learn()
+        if done:
+            break
+        s = s_
+
+    dqn.decay_epsilon()
+    print('Ep: ', i_episode,
+        '| Ep_r: ', round(ep_r, 2))
